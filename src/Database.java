95b80252678bcfc38a33cbfc3784d2b62de82dc4
Aaron Du <151097536+Duaarondu@users.noreply.github.com>
2025-11-17 19:49:22 -0500

    Added database class

---
+++ src/Database.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+class Database {
+    public static Connection connection;
+
+    public static void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/database_name?serverTimezone=EST", "root", "database28");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
