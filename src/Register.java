b2e84957b765deaf47e87d1aaad78ceda9c596eb
Raiyou04 <yraiyan@nyit.edu>
2025-11-25 15:14:17 -0500

    main customer page

--- src/Register.java
+++ src/Register.java
@@ -1,9 +1,14 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Register extends JFrame {
     private JPanel contentPane;
 
     private JPanel panel;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton createButton;
 
     public Register() {
         this.setContentPane(this.panel);
@@ -11,6 +16,17 @@ public class Register extends JFrame {
         this.setBounds(600, 200, 300, 300);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        SetUPCreateButton(); // Button to create account for new users
+
+    }
+
+    public void SetUPCreateButton() {
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
 
