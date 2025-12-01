5bbbff62b4e0d9b684cd8bd0b114fc8b9e758897
Aaron Du <151097536+Duaarondu@users.noreply.github.com>
2025-12-01 16:42:56 -0500

    Doing creator for employees

--- src/AdminMenu.java
+++ src/AdminMenu.java
@@ -1,2 +1,34 @@
-public class AdminMenu {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class AdminMenu extends JDialog {
+    private JPanel contentpane;
+    private JButton employeeCreateButton;
+    private JButton exitButton;
+
+    public AdminMenu() {
+        this.setContentPane(contentpane);
+        this.setTitle("Admin Menu");
+        this.setBounds(600, 200, 300, 300);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        setUpCreateEmployeeButtonAction();
+
+    }
+    public void setUpCreateEmployeeButtonAction() {
+        employeeCreateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                goToEmployeeCreator();
+            }
+        });
+    }
+    public void goToEmployeeCreator() {
+        this.dispose();
+        new EmployeeCreator();
+    }
 }
+
