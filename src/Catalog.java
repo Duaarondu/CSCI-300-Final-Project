0e87549cfcde3dfbda5988c58d62981c7f1a367c
Raiyou04 <yraiyan@nyit.edu>
2025-11-30 19:49:56 -0500

    main customer page

--- src/Catalog.java
+++ src/Catalog.java
@@ -7,19 +7,46 @@ public class Catalog extends JFrame {
     private JButton myOrderButton;
     private JPanel panel;
     private JTable table;
+    private JButton backButton;
 
     public Catalog() {
 
-            this.setContentPane(this.panel);
-            this.setTitle("Catalog");
-            this.setBounds(600, 200, 300, 300);
-            this.setVisible(true);
-            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            SetUpCartButton();
-            SetUpOrderButton();
-
+        this.setContentPane(this.panel);
+        this.setTitle("Catalog");
+        this.setBounds(600, 200, 300, 300);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        SetUpCartButton();
+        SetUpOrderButton();
+        SetUpBackButton();
 
     }
+
+//    public itemList() {
+//        this.setContentPane(this.panel);
+//        this.setTitle("Table Demo");
+//        this.setBounds(600, 200, 300, 300);
+//        this.setVisible(true);
+//        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//        this.populateTable();
+//    }
+//
+//    public void populateTable() {
+//        try {
+//            String query = "SELECT Appointments.appointment_time, Patients.patient_name, Doctors.doctor_name " +
+//                    "FROM Appointments " +
+//                    "JOIN Patients " +
+//                    "ON Appointments.patient_id = Patients.patient_id " +
+//                    "JOIN Doctors " +
+//                    "ON Appointments.doctor_id = Doctors.doctor_id";
+//            PreparedStatement stm = Database.connection.prepareStatement(query);
+//            ResultSet result = stm.executeQuery(query);
+//            table1.setModel(DbUtils.resultSetToTableModel(result));
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     public void SetUpCartButton() {
         cartButton.addActionListener(new ActionListener() {
@@ -38,6 +65,18 @@ public class Catalog extends JFrame {
 
             }
         });
+    }
+
+    public void SetUpBackButton() {
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new SignIn().setVisible(true);
+
+            }
+        });
+
     }
 
 
